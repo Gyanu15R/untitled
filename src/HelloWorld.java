4b6721c17cdcfef73f8459859f26ca33db0196c2
Gyanu15R <gyanendrakumar.infoicon@gmail.com>
2022-10-13 09:30:53 +0530

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        String theme = "Hello Im World" ;
+        System.out.println("I am passing this information--"+"\t"+theme);
+    }
+}
